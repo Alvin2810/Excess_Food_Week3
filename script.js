ef4587abadfb621998471cb7beb371f733a42e2d
Alvin2810 <48338964+Alvin2810@users.noreply.github.com>
2020-04-29 21:41:11 +0930

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+$('.donate').click(function(){
+   if($('#name').val() !== ''){
+      alert("The Donation has been Submitted");
+   }
+})});
+
+$(document).ready(function(){
+$('.upload-recipe').click(function(){
+   if($('#dishname').val() !== ''){
+      alert("Your Recipe has been Uploaded");
+   }
+})});
+
+$(document).ready(function(){
+  $('#output').click(function(){
+  $('#choosefile').trigger('click'); });
+});
+
+var loadFile = function(event) {
+  var output = document.getElementById('output');
+  output.src = URL.createObjectURL(event.target.files[0]);
+  output.onload = function() {
+    URL.revokeObjectURL(output.src)
+  }
+};
+
+$(document).ready(function(){
+  $("#choosefile").click(function(){
+    $("#choosefile").hide();
+  })});
+
+  function hideChoosefile(){
+    var choosefile = document.getElementById('choosefile');
+    choosefile.addEventListener('click', showPicture, false);
+  }
+  function showPicture() {
+     document.getElementById('output').style.display = "block";
+  }
+window.addEventListener('load', hideChoosefile, false)
